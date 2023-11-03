7e90e153bcef9f4d332fac311932c5f2426230a7
miittya <21solosolo@gmail.com>
2023-11-03 15:24:20 +0300

    task B

--- main.cpp
+++ main.cpp
@@ -4,44 +4,16 @@
 using namespace std;
 
 int main() {
-    int N;
-    int M;
-    int a;
-    int L, R;
-    int min;
-    int ans;
-    vector<int> vec;
-
-    cin >> N;
-    cin >> M;
-
-    for (int i = 0; i < N; ++i) {
-        cin >> a;
-        vec.push_back(a);
-    }
-
-    for (int j = 0; j < M; ++j) {
-        cin >> L;
-        cin >> R;
-        min = vec[L];
-        ans = -1;
-        int k = L + 1;
-        while (k <= R) {
-            if (vec[k] > min) {
-                ans = vec[k];
-            }
-            else if (vec[k] < min) {
-                ans = min;
-                min = vec[k];
-            }
-            ++k;
-        }
-
-        if (ans == -1) {
-            cout << "NOT FOUND" << endl;
-        }
-        else {
-            cout << ans << endl;
+    int a, b, c, d, m, n;
+    cin >> a >> b >> c >> d;
+    m = a*d + b*c;
+    n = b*d;
+    int max = (m>n) ? m : n;
+    for (int i = max; i > 0; --i) {
+        if (m%i == 0 && n%i == 0) {
+            m/=i;
+            n/=i;
         }
     }
+    cout << m << " " << n;
 }
